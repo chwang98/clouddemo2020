cd8a46a82de50a2ae04e3711f45686facb7495fb
wangjunjie <wang@wang.com>
2020-10-24 23:43:13 +0800

    order添加Eureka Client

--- cloud-consumer-order80/src/main/java/cn/codewjj/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/cn/codewjj/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package cn.codewjj.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
  **/
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
