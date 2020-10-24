f25bdd0ab1e851f20245cd3e57c7aedee7b098e0
wangjunjie <wang@wang.com>
2020-10-24 23:30:17 +0800

    添加Eureka Server

---
+++ cloud-eureka-server7001/src/main/java/cn/codewjj/springcloud/EurekaMain7001.java
@@ -0,0 +1,22 @@
+package cn.codewjj.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @program: cloud2020
+ * @description: Eureka启动类
+ * @author: Mr.Wang
+ * @create: 2020-10-24 23:27
+ **/
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
+
+
