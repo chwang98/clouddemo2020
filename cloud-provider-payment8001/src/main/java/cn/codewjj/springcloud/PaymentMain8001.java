fff824e9504d652e3b8b793d0ed2c3ce81d20f44
wangjunjie <wang@wang.com>
2020-10-24 23:37:50 +0800

    payment添加Eureka Client

--- cloud-provider-payment8001/src/main/java/cn/codewjj/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/cn/codewjj/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package cn.codewjj.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create 2020-10-24 15:15
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
